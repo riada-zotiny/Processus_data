727292e98e92ea96e7bb8e473310f48c718ae3f4
NazeemBestNPC <samimosta123@gmail.com>
2025-11-23 13:46:49 +0100

    fixing path in data loader and requirement file

--- data/data_loader.py
+++ data/data_loader.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from pathlib import Path
 
-def load_data(data_dir = "data/raw/synthetic_coffee_health_10000.csv"):
+def load_data(data_dir = "../data/raw/synthetic_coffee_health_10000.csv"):
 #
 #   Loads csv data
 #
@@ -11,9 +11,8 @@ def load_data(data_dir = "data/raw/synthetic_coffee_health_10000.csv"):
 #   returns :
 #       pandas dataFrame
 #
-
-    path = Path(data_dir)
-    data = pd.read_csv(path)
+    data_path = Path(data_dir)
+    data = pd.read_csv(data_path)
 
     return data
 
