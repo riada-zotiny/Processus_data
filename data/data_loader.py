dcebfb227aee980be3d8472f35f6e4ee220ac4e8
riada-zotiny <amalya.mourih@gmail.com>
2025-11-27 21:37:29 +0100

    little nit adjust Processuss data structure TODO : Finish train model tomorrow see all possible ways

---
+++ data/data_loader.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from pathlib import Path
+
+def load_data(data_dir = "../data/raw/synthetic_coffee_health_10000.csv"):
+#
+#   Loads csv data
+#
+#   args :
+#       data_dir : path to the data directory
+# 
+#   returns :
+#       pandas dataFrame
+#
+    data_path = Path(data_dir)
+    data = pd.read_csv(data_path)
+
+    return data
+
+
+def main():
+    #
+    #
+    #
+    print('use load_data() function to load the data ' \
+    '\nsynthetic_coffee_health_10000.csv will be automatically loaded if no path is specified')
